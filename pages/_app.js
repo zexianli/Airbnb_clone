6dffa757c1de807859a67ac246c2e147b6ccabdf
zexianliOSU <lizexi@oregonstate.edu>
2021-09-23 16:07:41 -0700

    Fixed type on route

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import ProgressBar from "@badrap/bar-of-progress"
 import Router from 'next/router';
 
 const progress = new ProgressBar({
-  size: 8,
+  size: 4,
   color: "#FE595E",
   className: "z-50",
-  delay: 100
+  delay: 0,
 });
 
-Router.events.on("routerChangeStart", progress.start);
+Router.events.on("routeChangeStart", progress.start);
 Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
