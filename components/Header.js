4f16386f029b169128d77e869e132ff674af6176
zexianliOSU <lizexi@oregonstate.edu>
2021-09-23 17:26:02 -0700

    Styling new header calendar

--- components/Header.js
+++ components/Header.js
@@ -47,46 +47,50 @@ function Header({placeHolder}) {
     }
 
     return (
-        <header className="sticky top-0 z-50 
-            grid grid-cols-3 bg-white shadow-md 
-            p-5 md:px-10">
-            {/* Left */}
-            <div onClick={() => router.push("/")} className="relative flex items-center h-10 cursor-pointer my-auto">
-                <Image
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
-                    layout="fill"
-                    objectFit="contain"
-                    objectPosition="left"
-                />
-            </div>
+        <header className="sticky top-0 z-50 flex flex-col">
+            <div className="grid grid-cols-3 shadow-md bg-white p-5 md:px-10">
+                {/* Left */}
+                <div onClick={() => router.push("/")} className="relative flex items-center h-10 cursor-pointer my-auto">
+                    <Image
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+                        layout="fill"
+                        objectFit="contain"
+                        objectPosition="left"
+                    />
+                </div>
 
-            {/* Middle */}
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input
-                    value={searchInput}
-                    onChange={(e) => setSearchInput(e.target.value)}
-                    className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" 
-                    type="text" 
-                    placeholder={placeHolder || "Start your search"}
-                />
-                
-                <SearchIcon className="hidden md:inline-flex h-8 
-                bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
-            </div>
+                {/* Middle */}
+                <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                    <input
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                        className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" 
+                        type="text" 
+                        placeholder={placeHolder || "Start your search"}
+                    />
+                    
+                    <SearchIcon className="hidden md:inline-flex h-8 
+                    bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
+                </div>
 
-            {/* Right */}
-            <div className="flex items-center space-x-4 justify-end text-gray-500">
-                <p className="hidden md:inline cursor-pointer">Become a host</p>
-                <GlobeAltIcon className="h-6 cursor-pointer"/>
+                {/* Right */}
+                <div className="flex items-center space-x-4 justify-end text-gray-500">
+                    <p className="hidden md:inline cursor-pointer">Become a host</p>
+                    <GlobeAltIcon className="h-6 cursor-pointer"/>
 
-                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                    <MenuIcon className="h-6 cursor-pointer"/>
-                    <UserCircleIcon className="h-6 cursor-pointer"/>
+                    <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                        <MenuIcon className="h-6 cursor-pointer"/>
+                        <UserCircleIcon className="h-6 cursor-pointer"/>
+                    </div>
                 </div>
             </div>
-
+            
             {searchInput && (
-                <div className="flex flex-col col-span-3 mx-auto mt-3">
+                // <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+                //     bg-white rounded-3xl shadow-lg">
+
+                <div className="flex
+                bg-white rounded-3xl shadow-lg">
                     <DateRangePicker 
                         ranges={[selectionRange]}
                         minDate={new Date()}
